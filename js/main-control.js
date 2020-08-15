fd40c476add48725ce917edd8a1dedb2b051bd5a
Eugene <group113jinn@gmail.com>
2020-08-15 15:16:13 +0300

    controls grid ready no buttons graphics

--- js/main-control.js
+++ js/main-control.js
@@ -40,57 +40,72 @@ function renderCanvas() {
     var strHtml = `
     <div class="canvas-container">
          <canvas id="mem-canvas" height="400" width="400"></canvas>
-         <textarea id="mem-text" cols="35" rows="1" oninput="handleText()"></textarea>
+       
     </div>
 
-    <div class="next-line-wrap">
+  
+    <div class="control-container">
+
+    <div class="box text-wrap">
+        <textarea id="mem-text" cols="35" rows="1" oninput="handleText()"></textarea>
+        </div>
+
+    <div class="box next-line-wrap">
         <span class="text-line-current" >Current Line: </span><span class="line-current">1</span>
         <button class="next-line"  onclick="onNextLine()">Next Line</button>
     </div>
 
-    <div font-size-container>
+    <div class="box font-size-wrap">
     <label for="font-size">Font-size:</label>
     <input type="range" id="font-size" name="size" min="8" max="50" value="40" onchange="showSize(this.value),setSize(this.value)" />
     <span id="size-span">40</span>
     </div>
 
-    <div>
+    <div class="box line-up-wrap">
     <button class="line-up" onclick="onLineUp()">Up</button>
+    </div>
+
+    <div class="box line-down-wrap">
     <button class="line-down" onclick="onLineDown()">Down</button>
     </div>
 
-    <div>
+    <div class="box align-left-wrap">
     <button class="align-left" onclick="onAlignLeft()">Left</button>
+    </div>
+
+    <div class="box align-center-wrap">
     <button class="align-center" onclick="onAlignCenter()">Center</button>
+    </div>
+
+    <div class="box align-right-wrap">
     <button class="align-right" onclick="onAlignRight()">Right</button>
     </div>
 
-    <div>
+    <div class="box font-wrap">
     <button class="font" onclick="onFontToggle()">Font</button>
     </div>
 
-    <div>
+    <div class="box clear-wrap">
     <button class="clear" onclick="onResetCanvas()">Clear</button>
     </div>
 
-    <div>
+    <div class="box stroke-wrap">
     <button class="stroke" onclick="onChangeStroke()">A</button>
     </div>
 
-    <form onsubmit="setColor(event)">
+    <form class=" box color-wrap" onsubmit="setColor(event)">
     <input type="color" id="color-tool" value="#0000ff" hidden >
     <button  onclick="colorUpdate()" class="color-btn">Color</button>
     </form>
 
-    <form onsubmit="setBcgColor(event)">
+    <form class="box bcgcolor-wrap" onsubmit="setBcgColor(event)">
     <input type="color" id="bcolor-tool" value="#00ff00" hidden >
     <button  onclick="bcgColorUpdate()" class="bcolor-btn">bColor</button>
     </form>
     
-    <div>
+    <div class="box download-wrap">
     <button class="down"><a href="#" onclick="downloadCanvas(this)" download="">Download</a></button>
     </div>
-    
     
     `
     document.querySelector('.ad-container').innerHTML = ``;
